dd9821f93251b3e5b7a82f008b376fec553589d4
mr-atho <mr.fightto@gmail.com>
2022-12-01 09:55:55 +0700

    Refactor: view

--- lang/id/text.php
+++ lang/id/text.php
@@ -10,13 +10,28 @@ return [
     | The following language lines are used in app.
     |
     */
-
+    'home'                          =>  'Beranda',
+    'collection'                    =>  'Koleksi',
+    'owner'                         =>  'Pemilik',
+    'shelter'                       =>  'Kandang',
+    'gallery'                       =>  'Galeri',
+    'contact'                       =>  'Hubungi',
+    'login'                         =>  'Masuk',
     'logout'                        =>  'Keluar',
+    'register'                      =>  'Daftar',
     'welcome'                       =>  'Selamat datang',
     'verification_cek'              =>  'Sebelum melanjutkan, harap periksa email Anda untuk melakukan verifikasi email.',
     'verification_sent'             =>  'Link verifikasi yang baru telah dikirim ke email Anda.',
     'verification_not_receive'      =>  'Jika tidak menerima email, tekan tombol dibawah ini untuk mendapatkan',
     'verification_request'          =>  'link verifikasi yang baru',
     'send_message'                  =>  'Kirim Pesan',
+
+    'name'                          =>  'Nama',
+    'email'                         =>  'Email',
+    'subject'                       =>  'Subjek',
+    'messages'                      =>  'Pesan',
+    'phone'                         =>  'Telp',
+    'links'                         =>  'Link',
+    'our_services'                  =>  'Layanan Kami'
 
 ];
