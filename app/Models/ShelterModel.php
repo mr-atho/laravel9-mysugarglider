e3c6aa1a59e72a1719c1b116038afe1c7168d4b6
Atho Simatupang <tsmtpng@gmail.com>
2022-12-26 02:13:03 +0700

    Refactor: Sugar Glider View, Shelter View

--- app/Models/ShelterModel.php
+++ app/Models/ShelterModel.php
@@ -19,4 +19,8 @@ class ShelterModel extends Model
     {
         return $this->hasMany(SugargliderModel::class, 'shelter_id');
     }
+
+    public function user() {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
